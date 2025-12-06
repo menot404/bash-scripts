1ca48bc1dcd802d8c0e80780f4fa188ca57d426e
Me_Not404 <m3not404@gmail.com>
2025-12-06 23:57:50 +0000

    Doc: Doc condition

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#
+# Check to make sure the user has entered exactly two arguments
 
 if [ $# -ne 2 ]; then
     echo "Usage: backup.sh <source_directory> <target_directory>"
     echo "Please try again"
     exit 1
 fi
+
+# Check to see if rsync is installed
 
 if ! command -v rsync > /dev/null 2>&1; then
     echo "This scripts requireres rsync to be installed"
